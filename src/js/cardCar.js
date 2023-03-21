defb9eeeee146b9a2ade5c14fadfff1060596440
Oscar Love <olabradalove@gmail.com>
2023-03-21 14:43:08 +0000

    Separate cardCar javascript

---
+++ src/js/cardCar.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    loadCarContent();
+}
+
+function loadCarContent() {
+    fetch('../../../../Static Data/Car Data/cars.json')
+        .then((response) => response.json())
+        .then((cars) => {
+            if (!('content' in document.createElement('template'))) {
+                console.error('Your browser does not support templates');
+                return;
+            }
+
+            const container = document.getElementById('carCardContainer');
+
+            cars.forEach((car) => {
+                const tmpl = document
+                    .getElementById('car-card-template')
+                    .content.cloneNode(true);
+
+                tmpl.querySelector('h3').innerText = car.name;
+                tmpl.querySelector('p').innerText = car.model;
+                tmpl.querySelector('img').src = car.imgSrc;
+
+                container.appendChild(tmpl);
+            })
+        }).catch((err) => console.error(err));
+}
