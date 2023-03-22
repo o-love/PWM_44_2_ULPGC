1859a458065798a67ccf2273761c30756fa6abfc
Oscar Love <olabradalove@gmail.com>
2023-03-22 17:14:15 +0000

    Gas Station Dynamic loading

---
+++ src/js/gasStation.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    loadTemplate("../html/templates/loggedOutHeader.html", "header")
+    loadTemplate("../html/templates/components/map.html", "mapBody")
+    loadTemplate("../html/templates/components/gasDescription.html", "descriptionBody")
+    loadTemplate("../html/templates/footer.html", "footer")
+
+    loadGasStationContent();
+}
+
+function loadGasStationContent() {
+    fetch('../../Static Data/Gas Station/gasStop.json')
+        .then((response) => response.json())
+        .then((gasStop) => {
+
+            document.getElementById('GasStationName').innerText = gasStop["Rótulo"];
+            document.getElementById('GasAddress').innerText = `${gasStop["Dirección"]}. ${gasStop["Municipio"]}, ${gasStop["C.P."]}`;
+
+            Object.keys(gasStop).filter(key => key.startsWith("Precio")).forEach(key => {
+                if (gasStop[key] === "") {
+                    return;
+                }
+
+                let element = document.createElement('li');
+                element.innerText = `${key.substring(7)}: ${gasStop[key]}`;
+
+                document.getElementById('pricesList').appendChild(element);
+            })
+        }).catch((err) => console.error(err));
+}
