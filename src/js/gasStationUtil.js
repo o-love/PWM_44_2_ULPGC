0f7f1f7fdfd3279f32ec8370a936a4a174707322
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2023-03-25 17:35:28 +0000

    clean code gasStationUtil

--- src/js/gasStationUtil.js
+++ src/js/gasStationUtil.js
@@ -3,20 +3,25 @@ function loadAllPricesInto(gasStop, list) {
         if (gasStop[key] === "") {
             return;
         }
-
         let element = document.createElement('li');
-        const cheapPriceGas = 1.55;
-        const expensivePriceGas = 2;
-        let price = parseFloat(gasStop[key].replace(',', '.').toLocaleString());
-
-        if (price < cheapPriceGas) {
-            element.innerHTML = `${key.substring(7)}: <span style="color: green"><strong>${gasStop[key]}€</strong></span>`;
-        } else if (price > cheapPriceGas && price < expensivePriceGas) {
-            element.innerHTML = `${key.substring(7)}: <span style="color: yellow"><strong>${gasStop[key]}€</strong></span>`;
-        } else {
-            element.innerHTML = `${key.substring(7)}: <span style="color: red"><strong>${gasStop[key]}€</strong></span>`;
-        }
+        element.innerHTML = changeColorPriceTag(gasStop,key)
 
         list.appendChild(element)
     });
+}
+
+function changeColorPriceTag(gasStop, key){
+    let element = document.createElement('li');
+    const cheapPriceGas = 1.35;
+    const expensivePriceGas = 2;
+    let price = parseFloat(gasStop[key].replace(',', '.').toLocaleString());
+
+    if (price < cheapPriceGas) {
+        element.innerHTML = `${key.substring(7)}: <span style="color: green"><strong>${gasStop[key]}€</strong></span>`;
+    } else if (price > cheapPriceGas && price < expensivePriceGas) {
+        element.innerHTML = `${key.substring(7)}: <span style="color: yellow"><strong>${gasStop[key]}€</strong></span>`;
+    } else {
+        element.innerHTML = `${key.substring(7)}: <span style="color: red"><strong>${gasStop[key]}€</strong></span>`;
+    }
+    return element.innerHTML;
 }
