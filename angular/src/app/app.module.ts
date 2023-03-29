f97bad92a08d32cea19c6e7946a0591dfcb10223
Oscar Love <olabradalove@gmail.com>
2023-03-29 09:11:53 +0100

    Restructure project

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './pages/header/header.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
