e5632a6aa2aa440f188e3f37b845c2e691734aa2
Oscar Love <olabradalove@gmail.com>
2023-03-29 09:36:00 +0100

    Place home page in pages folder

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomePageComponent} from "./home-page/home-page.component";
+import {HomePageComponent} from "./pages/home-page/home-page.component";
 
 const routes: Routes = [
   {path: '', component: HomePageComponent}
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './pages/header/header.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { HomePageComponent } from './home-page/home-page.component';
+import {HomePageComponent} from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
