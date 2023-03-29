848805e52baf4f1851db4a7e93f8389bc7fa7714
Oscar Love <olabradalove@gmail.com>
2023-03-29 10:11:22 +0100

    Add User Service with JSON

--- angular/src/app/services/user.service.ts
+++ angular/src/app/services/user.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../models/user.model";
+import {of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(protected http: HttpClient) {
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>('./assets/json/users.json')
+  }
 }
