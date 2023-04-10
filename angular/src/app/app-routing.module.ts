0937a2f4c7b7a62a134063d117449dd390b82744
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2023-04-10 12:21:16 +0100

    aboutUs template in progress

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePageComponent} from "./pages/home-page/home-page.component";
 import {AdminsUserListComponent} from "./pages/admins-user-list/admins-user-list.component";
+import {AboutUsComponent} from "./pages/about-us/about-us.component";
 
 const routes: Routes = [
   {path: '', component: HomePageComponent, pathMatch: "full"},
-  {path: 'adminPage', component: AdminsUserListComponent}
+  {path: 'adminPage', component: AdminsUserListComponent},
+  {path: 'aboutUs', component: AboutUsComponent}
 ];
 
 @NgModule({
--- angular/src/app/pages/about-us/about-us.component.ts
+++ angular/src/app/pages/about-us/about-us.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.sass']
 })
-export class AboutUsComponent {
+export class AboutUsComponent implements OnInit{
+  title = "About";
+  paragraphs : string[] | any;
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('../../assets/json/aboutUs.json').subscribe((data: any) => {
+      this.paragraphs = data.paragraphs;
+    });
+  }
 }
