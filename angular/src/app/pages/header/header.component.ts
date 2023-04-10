73d298dfe1704bb181c6b6f5f7e4ad8e90f0c3f6
César José Delgado Suárez <cjdsgithub@gmail.com>
2023-04-10 12:06:24 +0100

    updated header

--- angular/src/app/pages/header/header.component.ts
+++ angular/src/app/pages/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent {
+
+export class HeaderComponent implements OnInit{
+
+  Rol: String = "usuarioNoLogeado";
+
+  ngOnInit(): void {
+
+  }
 
 }
