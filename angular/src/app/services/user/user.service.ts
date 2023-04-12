32ddb4cc6c449b158a5d2f8f662aa14ac0f04eff
Oscar Love <olabradalove@gmail.com>
2023-04-12 08:51:26 +0100

    Refactor user service

--- angular/src/app/services/user/user.service.ts
+++ angular/src/app/services/user/user.service.ts
@@ -1,11 +1,11 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {User} from "../models/User/user.model";
+import {User} from "../../models/User/user.model";
 import {of} from "rxjs";
 
 // @ts-ignore
-import data from '../../assets/json/users.json';
+import data from '../../../assets/json/users.json';
 
 @Injectable({
   providedIn: 'root'
