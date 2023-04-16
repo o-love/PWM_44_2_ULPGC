a85b0001338ed9c63d8e6847e924d42b6662e9c1
César José Delgado Suárez <cjdsgithub@gmail.com>
2023-04-16 02:39:22 +0100

    fixed responsive on login on md and lg size

--- angular/src/app/pages/login/login/login.component.ts
+++ angular/src/app/pages/login/login/login.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.sass'],
+  host:{
+    class:"flex-grow-1 d-sm-flex justify-content-sm-center"
+  }
 })
 export class LoginComponent {
 
