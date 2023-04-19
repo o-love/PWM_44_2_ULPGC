17ecd3e173064ba60fe61262c733999fc103674a
Oscar Love <olabradalove@gmail.com>
2023-04-19 10:03:35 +0100

    Basic Map added in

--- angular/src/app/pages/GeneralComponents/map/map.component.ts
+++ angular/src/app/pages/GeneralComponents/map/map.component.ts
@@ -1,5 +1,9 @@
 import {Component, Input} from '@angular/core';
+import {View, Map} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
 import {GasStation} from "../../../models/GasStation/gas-station.model";
+
 
 @Component({
   selector: 'app-map',
@@ -8,6 +12,20 @@ import {GasStation} from "../../../models/GasStation/gas-station.model";
 })
 export class MapComponent {
 
-  @Input()
-  gasStations: GasStation[] = [];
+  @Input() gasStations: GasStation[] = [];
+  Map: Map | undefined;
+
+  ngOnInit() {
+    this.Map = new Map({
+      view: new View({
+        center: [0, 0],
+        zoom: 1,
+      }),
+      layers: [new TileLayer({
+        source: new OSM()
+      })],
+      target: 'ol-map',
+    });
+    console.log(this.Map);
+  }
 }
