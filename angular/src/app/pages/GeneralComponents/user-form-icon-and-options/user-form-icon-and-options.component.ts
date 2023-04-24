377db9cc36783d0e314c4586e982b5480e6a3343
César José Delgado Suárez <101063121+CesarJoseDelgadoSuarez@users.noreply.github.com>
2023-04-24 11:22:16 +0100

    updated logout functionality

--- angular/src/app/pages/GeneralComponents/user-form-icon-and-options/user-form-icon-and-options.component.ts
+++ angular/src/app/pages/GeneralComponents/user-form-icon-and-options/user-form-icon-and-options.component.ts
@@ -1,25 +1,32 @@
 import {Component, OnInit} from '@angular/core';
-import { User} from "../../../models/User/user.model";
+import {User} from "../../../models/User/user.model";
 import {UserService} from "../../../services/user/user.service";
-import {ActivatedRoute} from "@angular/router";
+import {AuthService} from "../../../services/auth/auth.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-form-icon-and-options',
   templateUrl: './user-form-icon-and-options.component.html',
   styleUrls: ['./user-form-icon-and-options.component.sass']
 })
-export class UserFormIconAndOptionsComponent implements OnInit{
+export class UserFormIconAndOptionsComponent implements OnInit {
   user: User | null | undefined;
-  constructor(private userService: UserService, private route: ActivatedRoute) {
+
+  constructor(private userService: UserService, private route: ActivatedRoute, private authService: AuthService, private router: Router) {
   }
 
   ngOnInit(): void {
     const userId: number = this.route.snapshot.params['number'];
-    console.log(typeof(userId));
+    console.log(typeof (userId));
 
     this.userService.getUserById(userId).subscribe((user: User | undefined) => {
       this.user = user ?? undefined;
     });
+  }
+
+  logOut() {
+    this.authService.logout()
+    this.router.navigate(["/"])
   }
 }
 
