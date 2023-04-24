b70035d4dbdfaa57efdd94384b106643670ce7bf
Oscar Love <olabradalove@gmail.com>
2023-04-24 12:11:09 +0100

    Fix Gas Station Info styles

--- angular/src/app/pages/gas-info-page/gas-info-page.component.ts
+++ angular/src/app/pages/gas-info-page/gas-info-page.component.ts
@@ -5,7 +5,11 @@ import {ActivatedRoute} from "@angular/router";
 @Component({
   selector: 'app-gas-info-page',
   templateUrl: './gas-info-page.component.html',
-  styleUrls: ['./gas-info-page.component.sass']
+  styleUrls: ['./gas-info-page.component.sass'],
+  host: {
+    id: "mainContent",
+    class: "flex-grow-1 d-sm-flex justify-content-sm-center hideOnMobile"
+  }
 })
 export class GasInfoPageComponent {
 
