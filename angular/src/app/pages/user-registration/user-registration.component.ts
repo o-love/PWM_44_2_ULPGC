3f3791b019652a478a5b2b6c0693606aee3065e7
César José Delgado Suárez <101063121+CesarJoseDelgadoSuarez@users.noreply.github.com>
2023-04-26 10:18:02 +0100

    created userRegistration component

---
+++ angular/src/app/pages/user-registration/user-registration.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registration',
+  templateUrl: './user-registration.component.html',
+  styleUrls: ['./user-registration.component.sass'],
+  host: {
+    id: "mainContent",
+    class: "flex-grow-1 d-sm-flex justify-content-sm-center hideOnMobile"
+  }
+})
+export class UserRegistrationComponent {
+  registration(){
+
+  }
+}
