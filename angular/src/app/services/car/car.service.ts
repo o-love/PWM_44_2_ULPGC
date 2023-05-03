59a8c330f7caed8f6886e5b4b5abf84ae946c4aa
Oscar Love <olabradalove@gmail.com>
2023-05-03 09:31:41 +0100

    Car service store car fixed. Remove problematic store car image.

--- angular/src/app/services/car/car.service.ts
+++ angular/src/app/services/car/car.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, of} from "rxjs";
+import {from, map, Observable, of} from "rxjs";
 import {CarModel} from "../../models/Car/car.model";
 import {FirestoreService} from "../firestore/firestore.service";
 import {AngularFireStorage} from "@angular/fire/compat/storage";
@@ -9,32 +9,30 @@ import {AngularFireStorage} from "@angular/fire/compat/storage";
 })
 export class CarService {
 
-  constructor(protected http: HttpClient, private service: FirestoreService, private storage: AngularFireStorage) { }
+  constructor(protected http: HttpClient, private service: FirestoreService) { }
   private collection = "cars"
   getCars() : Observable<CarModel[]> {
     return this.getAllCars();
   }
 
   storeCar(car: CarModel) : Observable<CarModel> {
-    return of({ id: "",
-      matricula: "", marca: "", modelo: "", combustible: "",
-      aceite_motor: "", aceite_direccion: "", agua_radiador: "",
-      agua_parabrisas: "", liquido_frenos: "", valculina_caja_cambios: "",
-      valvulina_diferencial: "", agua_bateria: "", filtro_aceite: "",
-      filtro_aire: "", filtro_combustible: "", presion_neumaticos: "",
-      correo_distribucion: "", correa_altenador: "", limpieza_bujias: "",
-      cambio_bujias: "", impuesto_circulacion: "", seguro_coche: "",
-      revision_itv: "", foto_coche_src: ""
-    });
-
+    const promise: Promise<any> = this.service.createDoc(this.collection, car);
+    return from(promise).pipe(map((res) => {
+      car.id = res.id;
+      return car;
+    }))
   }
 
   storeCarImage(file: File) : Observable<string> {
+    /*
     const path = `car-icon/${Math.random().toString(36).substring(2)}.${file.type}`;
     const storageRef = this.storage.ref(path);
 
     storageRef.put(file);
+
     return storageRef.getDownloadURL();
+     */
+    return of("faliure")
   }
 
   getAllCars(){
