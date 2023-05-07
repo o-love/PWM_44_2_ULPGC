ac7bf055a2a569211081c0e2e5299a4424495fb4
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2023-05-07 19:18:04 +0100

    fix car for icon

--- angular/src/app/pages/car-profile-view/car-form-icon-and-options/car-form-icon-and-options.component.ts
+++ angular/src/app/pages/car-profile-view/car-form-icon-and-options/car-form-icon-and-options.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {CarModel} from "../../../models/Car/car.model";
+import {CarService} from "../../../services/car/car.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-car-form-icon-and-options',
   templateUrl: './car-form-icon-and-options.component.html',
-  styleUrls: ['./car-form-icon-and-options.component.sass']
+  styleUrls: ['./car-form-icon-and-options.component.sass'],
+
 })
 export class CarFormIconAndOptionsComponent {
   model: CarModel = {
@@ -19,5 +22,24 @@ export class CarFormIconAndOptionsComponent {
     cambio_bujias: "", impuesto_circulacion: "", seguro_coche: "",
     revision_itv: "", foto_coche_src: ""
   }
+  constructor(private carService: CarService, private router: Router, private route: ActivatedRoute) {
 
+  }
+  navigateToAddReparation(): void {
+    let id = this.route.snapshot.params['carId'];
+    this.router.navigate(['formReparation', {carId: id}]);
+  }
+
+  navigateToAddPumping(): void {
+    let id = this.route.snapshot.params['carId'];
+    this.router.navigate(['formPumping', {carId: id}]); // TODO: Navigate to car page
+  }
+
+  ngOnInit(): void {
+    const carId = this.route.snapshot.params['carId'];
+    this.carService.getCarByID(carId).subscribe(car => {
+      console.log(car);
+      this.model = car as CarModel;
+    });
+  }
 }
