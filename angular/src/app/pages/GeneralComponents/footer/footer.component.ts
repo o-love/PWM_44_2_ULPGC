e7ab019e53fac21ab0cb5cf30b2cc21b1598a9a9
César José Delgado Suárez <101063121+CesarJoseDelgadoSuarez@users.noreply.github.com>
2023-05-07 21:05:30 +0100

    added logo to the webSite on header and footer

--- angular/src/app/pages/GeneralComponents/footer/footer.component.ts
+++ angular/src/app/pages/GeneralComponents/footer/footer.component.ts
@@ -9,5 +9,6 @@ import { Component } from '@angular/core';
   }
 })
 export class FooterComponent {
+  photo: string = "https://firebasestorage.googleapis.com/v0/b/pwm2023-fba58.appspot.com/o/assets%2FCaptura%20de%20pantalla%202023-05-07%20a%20las%2020.59.44.png?alt=media&token=92c847c3-db97-4ba6-aa80-3f7033733481";
 
 }
